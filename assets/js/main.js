f420172593697dbb260110abcee5dab8b7df66b9
Gabriel Martell <g4brielmartell@gmail.com>
2024-03-21 02:18:30 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,13 +9,13 @@ window.onload = function () {
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    //document.getElementById("main").style.marginLeft = "250px";
     document.getElementById("mainbody").style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+    //document.getElementById("main").style.marginLeft = "0";
     document.getElementById("mainbody").backgroundColor = "white";
 }
