23b73e6e4d0ad217c47bb28a037de20dd93b01c1
Gabriel Martell <g4brielmartell@gmail.com>
2024-04-27 04:30:04 -0400

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,17 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 window.onload = function () {
+    dropDown()
     let sidebarCloseButton = document.querySelector("#closeBtn");
     let sidebarOpenButton = document.querySelector("#openBtn");
-    let goBackButton = document.querySelector("#goBack");
+    let linkedinButton = document.querySelector("#linkedin_link");
+    let githubButton = document.querySelector("#github_link");
 
     sidebarOpenButton.addEventListener("click", openNav);
     sidebarCloseButton.addEventListener("click", closeNav);
-    goBackButton.addEventListener("click", goBackToAbout)
+    linkedinButton.addEventListener("click", linkedinButtonFunc)
+    githubButton.addEventListener("click", githubButtonFunc)
 };
+
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
@@ -20,6 +24,27 @@ function closeNav() {
   document.getElementById("main").style.marginLeft = "0";
 }
 
-function goBackToAbout() {
-  window.location.href = '../about.html'
+function dropDown() {
+  var dropdown = document.getElementsByClassName("dropdown-btn");
+  var i;
+  
+  for (i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var dropdownContent = this.nextElementSibling;
+      if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none";
+      } else {
+        dropdownContent.style.display = "block";
+      }
+    });
+  }
 }
+function linkedinButtonFunc() {
+  window.location.href = 'https://www.linkedin.com/in/gabriel-martell-8a0a04252/'
+}
+
+function githubButtonFunc() {
+  window.location.href = 'https://github.com/gabrielmartell'
+}
+
